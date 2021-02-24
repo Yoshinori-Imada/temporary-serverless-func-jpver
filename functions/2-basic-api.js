74edbaa086f582a0add13ebff0d1de187237be6a
Yoshinori Imada <48372228+Yoshinori-Imada@users.noreply.github.com>
2021-02-25 04:00:58 +0900

    cors error fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require('../assets/data');
 
 exports.handler = async (event, context, cb) => {
   return {
+    headers: {
+      'Accesss-Control-Allow-Origin': '*',
+    },
     statusCode: 200,
     body: JSON.stringify(items),
   };
